6e0fbce55d26d8e50d9201296367ffb39581e624
testttt <alaf820603@gmail.com>
2025-02-26 18:09:51 +0800

    🚀 修正 mongodb 伺服器問題

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongo = require("mongodb");
-const uri = "mongodb+srv://arttree63:5kfG2aY29nJqUnpg@cluster0.axodi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGO_URI;
+//const uri = "mongodb+srv://arttree63:5kfG2aY29nJqUnpg@cluster0.axodi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new mongo.MongoClient(uri);
 const taipeiTime = new Date().toLocaleString("zh-TW", { timeZone: "Asia/Taipei" });
 const moment = require("moment-timezone");
